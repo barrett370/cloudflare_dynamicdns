c2eaf83ff36b07b0bfad7340e1e410db9003c10d
Sam Barrett <barrett370@gmail.com>
2023-08-27 16:04:27 +0100

    log errors

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/barrett370/cloudflare_dynamicdns
 
 go 1.20
 
-require github.com/barrett370/crongo v0.0.0-20230826235559-2414b311afd2
+require github.com/barrett370/crongo v0.0.1
--- main.go
+++ main.go
@@ -36,13 +36,13 @@ func parseEnvironment() error {
 	return err
 }
 
-func startUpdateCrons() (crons []*crongo.Scheduler) {
+func startUpdateCrons(errc chan<- error) (crons []*crongo.Scheduler) {
 	for _, zone := range config.Config.Cloudflare {
 		syncWorkflow, err := workflow.NewCloudflareSyncWorkflow(zone.Authentication.APIToken, zone.ZoneName)
 		if err != nil {
 			log.Fatal(err)
 		}
-		updateCron := crongo.New(fmt.Sprintf("Cloudflare DynamicDNS Service [%s]", zone.ZoneName), syncWorkflow, time.Second*time.Duration(zone.IntervalSeconds))
+		updateCron := crongo.New(fmt.Sprintf("Cloudflare DynamicDNS Service [%s]", zone.ZoneName), syncWorkflow, time.Second*time.Duration(zone.IntervalSeconds), crongo.WithErrorsOut(errc))
 		updateCron.Start()
 		crons = append(crons, updateCron)
 	}
@@ -66,10 +66,28 @@ func main() {
 	if err != nil {
 		log.Fatalf("error loading environment, %v", err)
 	}
-	crons := startUpdateCrons()
+
+	// given to crongo scheduler to pass errors out
+	errc := make(chan error)
+	// used to signal end of errc processing
+	errLogCleanup := make(chan struct{})
+	go func() {
+		for err := range errc {
+			log.Printf("error from scheduler: %v", err)
+		}
+		close(errLogCleanup)
+	}()
+
+	crons := startUpdateCrons(errc)
 	interruptC := make(chan os.Signal, 1)
+
 	signal.Notify(interruptC, os.Interrupt, syscall.SIGTERM)
 	<-interruptC
+
 	log.Println("received os interrupt or kill, stopping update processes..")
 	stopUpdateCrons(crons)
+	// signal error logger to finish
+	close(errc)
+	// wait for error logger to finish
+	<-errLogCleanup
 }
